b4090b8172099cd22bbb1d68bcaa0e74ad850b3b
BogdanZmeul <bogdanzmeul@gmail.com>
2026-03-06 22:06:06 +0200

    Add Category api urls

--- ais/urls.py
+++ ais/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views.TestTableListView import TestTableListView
+from .views.CategoryView import CategoryListView
+from .views.CategoryView import CategoryDetailView
 
 urlpatterns = [
     path('test-data/', TestTableListView.as_view(), name='test-data-list'),
+    path('categories/', CategoryListView.as_view(), name='category-list'),
+    path('categories/<int:category_number>/', CategoryDetailView.as_view(), name='category-detail'),
 ]
