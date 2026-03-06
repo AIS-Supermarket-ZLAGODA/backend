3541d785de0d8fad9804201449e6871f5c468995
BogdanZmeul <bogdanzmeul@gmail.com>
2026-03-06 15:02:12 +0200

    Add CategorySerializer

---
+++ ais/serializers/CategorySerializer.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+class CategorySerializer(serializers.Serializer):
+    category_number = serializers.IntegerField(read_only=True)
+    category_name = serializers.CharField(max_length=255)
