174b0adf2424c0f326f6565c5036f265d2064ea9
BogdanZmeul <bogdanzmeul@gmail.com>
2026-03-07 20:39:25 +0200

    Fix CategorySerializer name filed max length to 50

--- ais/serializers/CategorySerializer.py
+++ ais/serializers/CategorySerializer.py
@@ -3,4 +3,4 @@ from rest_framework import serializers
 
 class CategorySerializer(serializers.Serializer):
     category_number = serializers.IntegerField(read_only=True)
-    category_name = serializers.CharField(max_length=255)
+    category_name = serializers.CharField(max_length=50)
