304954904fb652afadc0065a159eb1709022cd23
Danylo Bezukh <113092192+DaniloBez@users.noreply.github.com>
2026-04-01 23:00:25 +0300

    Fix: remove unused files

--- ais/urls.py
+++ ais/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from .views.TestTableListView import TestTableListView
 from .views.CategoryView import CategoryListView
 from .views.CategoryView import CategoryDetailView
 from .views.ProductView import ProductListView, ProductStatsView
@@ -12,7 +11,6 @@ from .views.StoreProductView import StoreProductListView, StoreProductDetailView
 
 urlpatterns = [
     path('auth/login/', LoginView.as_view(), name='auth-login'),
-    path('test-data/', TestTableListView.as_view(), name='test-data-list'),
     path('categories/', CategoryListView.as_view(), name='category-list'),
     path('categories/<int:category_number>/', CategoryDetailView.as_view(), name='category-detail'),
     path('products/', ProductListView.as_view(), name='product-list'),
